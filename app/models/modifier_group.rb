beff4d44755f17c1526c80ecf2454123954f1179
Wilson <imweesheng@gmail.com>
2024-02-27 23:00:51 +0800

    Update validation logic for modifier_group:
    - To consider a scenario where min can be at least 1 when max is 0
    
    - Retain min cannot be greater than max when max is not 0

--- app/models/modifier_group.rb
+++ app/models/modifier_group.rb
@@ -5,14 +5,16 @@ class ModifierGroup < ApplicationRecord
   validates :label, uniqueness: true
   validates :selection_required_min, numericality: { greater_than_or_equal_to: 0 }
   validates :selection_required_max, numericality: { greater_than_or_equal_to: 0 }
-  validate :selection_required_min_not_greater_than_max
+  validate :selection_required_range
 
   private
 
-  def selection_required_min_not_greater_than_max
+  def selection_required_range
     return if selection_required_min.blank? && selection_required_max.blank?
 
-    if selection_required_min > selection_required_max
+    if selection_required_max.zero?
+      errors.add(:selection_required_min, "must be greater than or equal to 0 when selection_required_max is 0") if selection_required_min < 0
+    elsif selection_required_min > selection_required_max
       errors.add(:selection_required_min, "cannot be greater than selection_required_max")
     end
   end
